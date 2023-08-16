73b182e03af482e5a8f842e2b3d9a22bb234f35d
Michael Adamson <michaelnjadamson@outlook.com>
2023-08-16 14:48:09 -0400

    changes made. fixing warning

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,10 +1,55 @@
 import React from 'react'
 
 export default class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      todos: [
+        {
+          task: 'Organize Garage',
+          id: 1528817077286,
+          completed: false
+        },
+        {
+          task: 'Bake Cookies',
+          id: 1528817084358,
+          completed: false
+        },
+        {
+          task: 'Walk The Dog',
+          id: 1528817084358,
+          completed: false
+        },
+        {
+          task: 'Take out the Trash',
+          id: 1528817084358,
+          completed: false
+        }
+      ]
+    }
+  }
   render() {
+    const{ todos } =this.state;
+    console.log(todos);
+
     return (
       <div>
-        Todo App
+        <h1>Todos</h1>
+
+        <ul>
+          {
+            todos.map(todo => {
+              return (<li>{todo.task} { todo.completed?<span>- completed</span> : <span></span>} </li>)
+            })
+          }
+        </ul>
+
+        <form>
+          <input/>
+          <button>App</button>
+        </form>
+
+        <button>Clear</button>
       </div>
     )
   }
